7dd3ae37646e079a17f177332adbf8fbc2098fed
EllenLis <elena139@bk.ru>
2022-10-28 09:01:06 +0300

    Added gitignore file

---
+++ UIScrollView_UITableViewDelegate_Lysenskaia/Profile/ProfileViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ProfileViewController.swift
+//  UIScrollView_UITableViewDelegate_Lysenskaia
+//
+//  Created by ElenaL on 28.10.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    private lazy var profileTableView : UITableView = {
+        
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: String(describing: PostTableViewCell.self))
+        return tableView
+    }()
+    
+    let massivFeed = Post.postFeed()
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        view.addSubview(profileTableView)
+        
+        NSLayoutConstraint.activate([
+            profileTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            profileTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            profileTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            profileTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+    
+}
+
+extension ProfileViewController : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        massivFeed.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: PostTableViewCell.self), for: indexPath) as! PostTableViewCell
+        cell.setup(massivFeed[indexPath.row])
+        return cell
+    }
+}
+
+extension ProfileViewController : UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = ProfileHeaderView()
+        return header
+    }
+}
